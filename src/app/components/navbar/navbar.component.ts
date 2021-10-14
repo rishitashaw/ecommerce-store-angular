249c218c05400e43644c9c1e83b0258520f04d06
Theseregrets <rishitashaw49@gmail.com>
2021-10-14 12:38:45 +0530

    making navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faShippingFast, faHeart } from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  faShippingFast = faShippingFast
+  faHeart = faHeart
   constructor() { }
 
   ngOnInit(): void {
